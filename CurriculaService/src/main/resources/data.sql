3a5e44e8a8aa1726721fbd5350fe40aeea62a6c4
Abatson <abatson94@gmail.com>
2020-01-09 13:46:21 -0500

    fixed h2 script

--- CurriculaService/src/main/resources/data.sql
+++ CurriculaService/src/main/resources/data.sql
@@ -1,1 +1,30 @@
-insert into test (test_name) values ('Hello');
+insert into category
+	values (1, 'framework'),
+			(2, 'devops'),
+			(3, 'architecture'),
+			(4, 'database'),
+			(5, 'sourcecode'),
+			(6, 'ide');
+		
+insert into skill
+	values (1, 'Angular', 1),
+			(2, 'Jenkins', 2),
+			(3, 'MVC', 3),
+			(4, 'Postgres', 4),
+			(5, 'git', 5),
+			(6, 'Eclipse', 6),
+			(7, 'Spring', 1),
+			(8, 'Hibernate', 1),
+			(9, 'Maven', 2),
+			(10, 'npm', 2),
+			(11, 'Tomcat', 2),
+			(12, 'SOA', 3),
+			(13, 'Microservices', 3),
+			(14, 'Containers', 3),
+			(15, 'Oracle', 4),
+			(16, 'RDS', 4),
+			(17, 'GitHub', 5),
+			(18, 'IntelliJ', 6),
+			(19, 'Visual Studio Code', 6),
+			(20, 'Postman', 6),
+			(21, 'React', 1);
--- CurriculaService/src/main/resources/schema.sql
+++ CurriculaService/src/main/resources/schema.sql
@@ -1,4 +1,70 @@
-create table test(
-id serial primary key,
-test_name text
+-- tables
+-- Table: Category
+CREATE TABLE category (
+    category_id serial  NOT NULL,
+    category_name varchar  NOT NULL UNIQUE,
+    CONSTRAINT category_pk PRIMARY KEY (category_id)
 );
+CREATE TABLE curriculum (
+    curriculum_id serial NOT NULL,
+    curriculum_name varchar NOT NULL UNIQUE,
+    CONSTRAINT curriculum_pk PRIMARY KEY (curriculum_id)
+);
+-- Table: Curriculum_Skill
+CREATE TABLE curriculum_skill (
+    curriculum int  NOT NULL,
+    skill int  NOT NULL,
+    CONSTRAINT curriculum_skill_pk PRIMARY KEY (curriculum, skill)
+);
+
+-- Table: Skill
+CREATE TABLE skill (
+    skill_id serial  NOT NULL,
+    skill_name varchar  NOT NULL UNIQUE,
+    category int  NOT NULL,
+    CONSTRAINT skill_pk PRIMARY KEY (skill_id)
+);
+
+-- Table: Visualization
+CREATE TABLE visualization (
+    visualization_id serial  NOT NULL,
+    visualization_name varchar  NOT NULL UNIQUE,
+    CONSTRAINT visualisation_pk PRIMARY KEY (visualization_id)
+);
+
+-- Table: Visualization_Curriculum
+CREATE TABLE visualization_curriculum (
+    visualization int  NOT NULL,
+    curriculum int  NOT NULL,
+    CONSTRAINT visualization_curriculum_pk PRIMARY KEY (visualization, curriculum)
+);
+
+-- foreign keys
+-- Reference: Curr_Cat_Curriculum (table: Curriculum_Skill)
+ALTER TABLE curriculum_skill ADD CONSTRAINT curr_cat_curriculum
+    FOREIGN KEY (curriculum)
+    REFERENCES curriculum (curriculum_id)  
+;
+-- Reference: Curr_Cat_Skill (table: Curriculum_Skill)
+ALTER TABLE curriculum_skill ADD CONSTRAINT curr_cat_skill
+    FOREIGN KEY (skill)
+    REFERENCES skill (skill_id)  
+;
+
+-- Reference: Skill_Category (table: Skill)
+ALTER TABLE skill ADD CONSTRAINT skill_category
+    FOREIGN KEY (category)
+    REFERENCES category (category_id)  
+;
+
+-- Reference: Vis_Curr_Curriculum (table: Visualization_Curriculum)
+ALTER TABLE visualization_curriculum ADD CONSTRAINT vis_curr_curriculum
+    FOREIGN KEY (curriculum)
+    REFERENCES curriculum (curriculum_id)  
+;
+
+-- Reference: Vis_Curr_Visualization (table: Visualization_Curriculum)
+ALTER TABLE visualization_curriculum ADD CONSTRAINT vis_curr_visualization
+    FOREIGN KEY (visualization)
+    REFERENCES visualization (visualization_id)  
+;
