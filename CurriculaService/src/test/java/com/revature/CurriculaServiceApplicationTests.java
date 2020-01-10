b8750977c2fb942fe51942ed7653363c27dd933f
Kenneth Davis <kennethdavis391@gmail.com>
2020-01-09 19:01:54 -0500

    push to pull from dev

--- CurriculaService/src/test/java/com/revature/CurriculaServiceApplicationTests.java
+++ CurriculaService/src/test/java/com/revature/CurriculaServiceApplicationTests.java
@@ -1,13 +1,25 @@
 package com.revature;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.util.LinkedList;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CurriculaServiceApplicationTests {
 
+	
+	
 	@Test
 	void contextLoads() {
+		LinkedList<Integer> mockedList = mock(LinkedList.class);
+		
+		when(mockedList.contains(1)).thenReturn(true);
+		
+		assertEquals(mockedList.contains(1),true);
 	}
 
 }
